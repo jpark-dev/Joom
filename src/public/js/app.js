7a4c0f000e1d0dbe6dd85238ed0dfe0cc5a13fd6
Jason Park <zeipar@gmail.com>
2023-08-04 17:08:49 -0700

    Added a form to set a nickname with server logic. Refactored accordingly.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,25 +1,40 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nicknameForm = document.querySelector("#nickname");
 
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+const constructMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
 
 socket.addEventListener("open", () => {
   console.log("*** Connected to the Server!! ***");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("Admin >>", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("*** Disconnected from the Server!! ***");
 });
 
-const handleSubmit = (event) => {
+const handleMessageSubmit = (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(constructMessage("new_message", input.value));
   input.value = "";
 };
 
-messageForm.addEventListener("submit", handleSubmit);
+const handleNicknameSubmit = (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(constructMessage("nickname", input.value));
+};
+
+messageForm.addEventListener("submit", handleMessageSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
